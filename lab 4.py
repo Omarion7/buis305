c34349831a2636875ccb22102b9658d6d696c970
Mari <omarionwood@gmail.com>
2023-11-01 14:24:53 -0400

    Lab3Files

---
+++ lab 4.py
@@ -0,0 +1,20 @@
+import math
+
+for i in range(0,11):
+ print(i)
+for i in range(1,11):
+    print(i)
+for i in range(1,11,2):
+    print(i)
+radius=float(input('enter radius'))
+area=math.pi*radius*radius
+print(area)
+length=float(input('enter length'))
+print(length)
+breadth=float(input('enter breadth'))
+print(breadth)
+area=length*breadth
+print(area)
+
+
+
